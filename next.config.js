878d6bcd23bb8846281a5351012f5128d4ae8ea9
cherryiu <116459705+cherryiu@users.noreply.github.com>
2024-01-28 14:22:33 -0600

    removed github workflows

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/portfolio-app-1",
-    output: "export", 
-    reactStrictMode: true,
 };
 
 module.exports = nextConfig
